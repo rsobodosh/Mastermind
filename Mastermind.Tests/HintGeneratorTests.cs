e0cf38a20902c05a82b5528f4932067d946d616d
rsobodosh <rsobodosh@gmail.com>
2025-08-09 14:31:46 -0400

    Got HintGenerator working with tests

---
+++ Mastermind.Tests/HintGeneratorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind.Tests
+{
+    internal class HintGeneratorTests
+    {
+        [Test]
+        public void Test_AllCorrect()
+        {
+            Assert.That(HintGenerator.GeneratorHint("1234", "1234"), Is.EqualTo("++++"));
+        }
+
+        [Test]
+        public void Test_AllIncorrect()
+        {
+            Assert.That(HintGenerator.GeneratorHint("1234", "5566"), Is.EqualTo(""));
+        }
+
+        [Test]
+        public void Test_SomeCorrect()
+        {
+            Assert.That(HintGenerator.GeneratorHint("1234", "1243"), Is.EqualTo("++--"));
+        }
+
+        [Test]
+        public void Test_AllCorrectJustInWrongPositions()
+        {
+            Assert.That(HintGenerator.GeneratorHint("1234", "4321"), Is.EqualTo("----"));
+        }
+
+        [Test]
+        public void Test_ExampleFromDirections()
+        {
+            Assert.That(HintGenerator.GeneratorHint("1234", "4233"), Is.EqualTo("++-"));
+        }
+    }
+}
---
+++ Mastermind/HintGenerator.cs
@@ -0,0 +1,41 @@
+﻿namespace Mastermind
+{
+    public class HintGenerator
+    {
+        public static string GeneratorHint(string secretAnswer, string input)
+        {
+            var hint = AddPlusSymbols(secretAnswer, input);
+            hint = AddMinusSymbols(secretAnswer, input, hint);
+
+            return string.Join("", hint.Values);
+        }
+
+        public static Dictionary<char, string> AddPlusSymbols(string secretAnswer, string input)
+        {
+            Dictionary<char, string> hint = new Dictionary<char, string>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (input[i] == secretAnswer[i] && !hint.ContainsKey(input[i]))
+                {
+                    hint.Add(input[i], "+");
+                }
+            }
+
+            return hint;
+        }
+
+        public static Dictionary<char, string> AddMinusSymbols(string secretAnswer, string input, Dictionary<char, string> hint)
+        {
+            foreach (char c in input)
+            {
+                if (secretAnswer.Contains(c) && !hint.ContainsKey(c))
+                {
+                    hint.Add(c, "-");
+                }
+            }
+
+            return hint;
+        }
+    }
+}
